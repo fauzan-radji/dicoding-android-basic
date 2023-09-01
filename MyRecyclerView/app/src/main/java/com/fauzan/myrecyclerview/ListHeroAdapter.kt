1f91a7529192c7f60cf068b41f0edfaec221eddf
Fauzan Radji <triputrafauzan@gmail.com>
2023-09-01 20:38:20 +0800

    latihan recyclerview

---
+++ MyRecyclerView/app/src/main/java/com/fauzan/myrecyclerview/ListHeroAdapter.kt
@@ -0,0 +1,47 @@
+package com.fauzan.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemCLickCallback
+
+    interface OnItemCLickCallback {
+        fun onItemClicked(data: Hero)
+    }
+
+    fun setOnItemClickCallback(callback: OnItemCLickCallback) {
+        this.onItemClickCallback = callback
+    }
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val hero = listHero[position]
+        val (name, description, photo) = hero
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+
+        holder.itemView.setOnClickListener {
+            onItemClickCallback.onItemClicked(hero)
+        }
+    }
+}
